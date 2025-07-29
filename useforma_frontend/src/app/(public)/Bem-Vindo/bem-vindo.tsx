ded2228e27423afb3fc8154c687ed15228ab1e5e
DevLucasFontoura <devlucasfontoura@gmail.com>
2025-07-29 16:47:50 -0300

    backup

--- useforma_frontend/src/app/(public)/Bem-Vindo/bem-vindo.tsx
+++ useforma_frontend/src/app/(public)/Bem-Vindo/bem-vindo.tsx
@@ -1,3 +1,4 @@
+import { BEM_VINDO_CONST } from '@/common/constants/bem-vindo.const';
 import PublicMenu from '@/components/publicMenu/public-menu';
 import styles from './bem-vindo.module.css';
 
@@ -16,15 +17,11 @@ export default function BemVindo() {
           {/* Header */}
           <div className={styles.header}>
             <div className={styles.titleContainer}>
-              <div className={styles.titleUse}>USE</div>
-              <h1 className={styles.titleForma}>FORMA</h1>
+              <div className={styles.titleUse}>{BEM_VINDO_CONST.TITULO_SISTEMA_01}</div>
+              <h1 className={styles.titleForma}>{BEM_VINDO_CONST.TITULO_SISTEMA_02}</h1>
             </div>
-            <p className={styles.subtitle}>
-              Seu sistema completo de gestão e formação. Transforme sua experiência de aprendizado.
-            </p>
-            <p className={styles.description}>
-              Plataforma inovadora que combina tecnologia avançada com metodologias comprovadas para maximizar seu desenvolvimento profissional e pessoal.
-            </p>
+            <p className={styles.subtitle}>{BEM_VINDO_CONST.SUBTITULO_SISTEMA}</p>
+            <p className={styles.description}>{BEM_VINDO_CONST.DESCRICAO_SISTEMA}</p>
           </div>
         </div>
       </div>
---
+++ useforma_frontend/src/common/constants/bem-vindo.const.ts
@@ -0,0 +1,8 @@
+export const BEM_VINDO_CONST = {
+    TITULO_SISTEMA: 'USE FORMA',
+    TITULO_SISTEMA_01: 'USE',
+    TITULO_SISTEMA_02: 'FORMA',
+    SUBTITULO_SISTEMA: 'Seu sistema completo de gestão e formação. Transforme sua experiência de aprendizado.',
+    DESCRICAO_SISTEMA: 'Plataforma inovadora que combina tecnologia avançada com metodologias comprovadas para maximizar seu desenvolvimento profissional e pessoal.',
+
+};
