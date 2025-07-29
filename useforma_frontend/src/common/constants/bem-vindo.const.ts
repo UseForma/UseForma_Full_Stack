abd62979daf52ee436e3a8b71dc005504ff03896
DevLucasFontoura <devlucasfontoura@gmail.com>
2025-07-29 16:57:18 -0300

    backup

--- useforma_frontend/src/common/constants/bem-vindo.const.ts
+++ useforma_frontend/src/common/constants/bem-vindo.const.ts
@@ -2,7 +2,5 @@ export const BEM_VINDO_CONST = {
     TITULO_SISTEMA: 'USE FORMA',
     TITULO_SISTEMA_01: 'USE',
     TITULO_SISTEMA_02: 'FORMA',
-    SUBTITULO_SISTEMA: 'Seu sistema completo de gestão e formação. Transforme sua experiência de aprendizado.',
-    DESCRICAO_SISTEMA: 'Plataforma inovadora que combina tecnologia avançada com metodologias comprovadas para maximizar seu desenvolvimento profissional e pessoal.',
-
+    SUBTITULO_SISTEMA: 'Sistema completo para personal trainers. Controle seus alunos, treinos e resultados com eficiência total.',
 };
